e5fe5cf49c6be52ef69b3e8f119e1167939455a6
Angelo Luciani <mistereal@gmail.com>
2016-05-19 17:34:03 +0200

    building_an_image_from_dockerfile
    building_an_image_from_dockerfile

---
+++ building_an_image_from_dockerfile.sh
@@ -0,0 +1,9 @@
+cd static_web 
+$ sudo docker build -t= "jamtur01/static_web" .
+Sending build context to Docker daemon 2.56 kB 
+Sending build context to Docker daemon 
+Step 0 : FROM ubuntu:14.04 
+---> ba5877dc9bec 
+Step 1 : MAINTAINER Angelo Luciani "mistereal@gmail.com" 
+---> Running in b8ffa06f9274 
+---> 4c66c9dcee35
